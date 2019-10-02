6a2f41c0156ca95fc3d0ccb8b4a662af67866044
markcardamis <markncardamis@gmail.com>
2019-10-03 01:11:20 +1000

    initial commit

---
+++ src/main/java/com/majoapps/reservations/landon/data/respository/GuestRepository.java
@@ -0,0 +1,11 @@
+package com.majoapps.reservations.landon.data.respository;
+
+import com.majoapps.reservations.landon.data.entity.Guest;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends PagingAndSortingRepository<Guest, Long> {
+    //Guest findOne(Long id);
+}
---
+++ src/main/java/com/majoapps/reservations/landon/data/respository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package com.majoapps.reservations.landon.data.respository;
+
+import com.majoapps.reservations.landon.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    List<Reservation> findByDate(Date date);
+}
