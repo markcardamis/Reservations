916ccfa194e4875df146badb65f263909fa076d9
markcardamis <markncardamis@gmail.com>
2019-10-05 18:16:05 +1000

    added Room Web and API Service

---
+++ src/main/java/com/majoapps/reservations/landon/business/service/RoomService.java
@@ -0,0 +1,36 @@
+package com.majoapps.reservations.landon.business.service;
+
+
+import com.majoapps.reservations.landon.data.entity.Room;
+import com.majoapps.reservations.landon.data.respository.RoomRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getRooms(String roomNumber){
+        List<Room> rooms = new ArrayList<>();
+        if (roomNumber == null){
+            Iterable<Room> results = this.roomRepository.findAll();
+            results.forEach(room -> {rooms.add(room);});
+        } else {
+            Room room = this.roomRepository.findByNumber(roomNumber);
+            if (room != null) {
+                rooms.add(room);
+            }
+        }
+        return rooms;
+    }
+
+}
--- src/main/java/com/majoapps/reservations/landon/web/service/RoomServiceController.java
+++ src/main/java/com/majoapps/reservations/landon/web/service/RoomServiceController.java
@@ -1,37 +1,34 @@
-package com.majoapps.reservations.landon.web.application;
+package com.majoapps.reservations.landon.web.service;
 
 import com.majoapps.reservations.landon.business.service.RoomService;
 import com.majoapps.reservations.landon.data.entity.Room;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping(value="/rooms")
-public class RoomController {
+@RestController
+@RequestMapping(value="/api")
+public class RoomServiceController {
 
     @Autowired
     private RoomService roomService;
 
     @Autowired
-    public RoomController(RoomService roomService){
+    public RoomServiceController (RoomService roomService){
         super();
         this.roomService = roomService;
     }
 
-
-    @RequestMapping(method= RequestMethod.GET)
-    public String getRooms(@RequestParam(value="roomNumber", required=false)String roomNumber, Model model){
-        List<Room> getRoomById = this.roomService.getRooms(roomNumber);
-        model.addAttribute("rooms", getRoomById);
+    @RequestMapping(value="/rooms/{roomNumber}", method= RequestMethod.GET)
+    public List<Room> getRoomById(@RequestParam(required=false) String roomNumber) {
+    //public List<Room> getRoomById(@PathVariable(value="roomNumber") String roomNumber) {
         System.out.println(roomNumber);
-        return "rooms";
+        return this.roomService.getRooms(roomNumber);
     }
 
 }
